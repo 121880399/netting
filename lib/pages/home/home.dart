59ccd75219e07fdd0d9b866d0c0c1ca16df6473f
zhouzhengyi <zhouzhengyi@01zhuanche.com>
2019-06-27 17:54:04 +0800

    1.编写首页

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -6,8 +6,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String image = "";
+  List<Widget> listData;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          new Image.asset(
+            image,
+            height: 400.0,
+          ),
+          new ListView.builder(
+            itemCount: listData == null ? 0 : listData.length,
+            itemBuilder: (BuildContext context,int
+          position){
+            return _getView(context,position);
+          },
+
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _getView(BuildContext context,int position){
+
   }
 }
